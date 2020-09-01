03ffa891d008076414462daf79cce6b4fb01bed5
tipie101 <timka.timkah@gmail.com>
2020-09-02 00:56:45 +0200

    cors communication implemented

--- paranoia-walk-frontend/src/pages/index.tsx
+++ paranoia-walk-frontend/src/pages/index.tsx
@@ -13,39 +13,18 @@ import { Container } from '../components/Container'
 import { Main } from '../components/Main'
 import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
+import { ParanoidMap } from '../components/ParanoidMap'
+import { LeafletMap } from '../components/LeafletMap'
 
 const Index = () => (
   <Container>
     <Hero />
     <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
-
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <Icon name="external-link" mx="2px" />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <Icon name="external-link" mx="2px" />
-          </ChakraLink>
-        </ListItem>
-      </List>
+    <ParanoidMap></ParanoidMap>
     </Main>
     <Footer>
-      <Text>Next ❤️ Chakra</Text>
+      <Text>❤️</Text>
     </Footer>
-    <CTA />
   </Container>
 )
 
--- risk-calc-service/src/main.ts
+++ risk-calc-service/src/main.ts
@@ -4,8 +4,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { __port__ } from './statics/constants';
 
+const whitelist = ['http://localhost:3000', 'http://localhost:4000'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      // see: https://stackoverflow.com/questions/42589882/nodejs-cors-middleware-origin-undefined
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+    });
   await app.listen(__port__);
 }
 bootstrap();
